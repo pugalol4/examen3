caa2a010be891ab895975d900307423f75aca206
alejandro puga <alejandropuga1999@hotmail.com>
2019-06-11 13:12:42 -0500

    mensaje de bienvenida

--- examen3.c
+++ examen3.c
@@ -1,58 +1,36 @@
-#include <calculadora_display.h>
-#include <libreria1.h>
-#include <libreria2.h>
+#include <18F4620.h>
+
+#FUSES NOWDT                    //No Watch Dog Timer
+#FUSES NOFCMEN                  //Fail-safe clock monitor disabled
+#FUSES NOIESO                   //Internal External Switch Over mode disabled
+#FUSES PUT                      //Power Up Timer
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES NOLPT1OSC                //Timer1 configured for higher power operation
+#FUSES NOSTVREN                 //Stack full/underflow will not cause reset
+#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+#FUSES NOXINST                  //Extended set extension and Indexed Addressing mode disabled (Legacy mode)
+
+#use delay(crystal=40MHz)
+#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)
 
 char caracter[22];//{-2+-2:}
 int flag=0;
 int bandera1=0;
-float numero1=0, numero2=0;
-float resultado=0;
 int calculo=0;
-int puntero=0;
-float decimal1=0,decimal2=0;
-char operacion;
-//void clean(void);
 int i=0;
-int clear=0;
-#int_RDA
-void RDA_isr(void){
-      if(kbhit()){
-               
-                  caracter[i]=getch();
-                  
-                  if(caracter[0]=='*' || caracter[0]=='/'){caracter[i]=1;}
-                  if(i>1 && caracter[i-1]=='*' && caracter[i]==caracter[i-1] || caracter[i-1]=='/' && caracter[i]==caracter[i-1] || caracter[i-1]=='/' && caracter[i]=='*' || caracter[i-1]=='*' && caracter[i]=='/'){caracter[i]='=';}
-                  if(i>1 && caracter[i-1]=='+' && caracter[i]==caracter[i-1] || caracter[i-1]=='-' && caracter[i]==caracter[i] && caracter[i-2]=='+'){caracter[i]='=';}
-                  if(i>1 && caracter[i-1]=='.' && caracter[i]==caracter[i-1]){caracter[i]=1;}       
-              
-              if(caracter[i]>=42 && caracter[i]<=62){
-                  printf("%c",caracter[i]);
-                  if(caracter[i]=='='){
-                  calculo=1;
-              }
-              i++;
-              bandera1=0;
-          }
-         }
-   }
+
+
 void main()
 {
-   printf("\fIngresa los datos siguiendo el orden dato,dato,operacion=\n");
+   printf("BIENVENIDO /r/n");
+   
    while(TRUE)
    {
    if(kbhit()){
          
             caracter[i]=getch();
-            
-       /*     if(caracter[0]=='*' || caracter[0]=='/'){caracter[i]=1;}
-            if(caracter[0]=='-' && caracter[1]==caracter[0] || caracter[0]=='+' && caracter[1]==caracter[0]){caracter[i]=1;}
-          //  if(i>1 && caracter[i]=='+' && caracter[i-1]==caracter[i]-1 ){caracter[i]==1;}
-            if(i>1 && caracter[i-1]=='*' && caracter[i]==caracter[i-1] || caracter[i-1]=='/' && caracter[i]==caracter[i-1] || caracter[i-1]=='/' && caracter[i]=='*' || caracter[i-1]=='*' && caracter[i]=='/'){caracter[i]=1;}
-            if(i>1 && caracter[i-1]=='+' && caracter[i]==caracter[i-1] || caracter[i-1]=='-' && caracter[i]==caracter[i-1] || caracter[i-1]=='-' && caracter[i]==caracter[i-1] && caracter[i-2]=='+'){caracter[i]=1;}
-            if(i>1 && caracter[i-1]=='.' && caracter[i]==caracter[i-1]){caracter[i]=1;}       
-        */
         
-        if(caracter[i]=='R' || caracter[i]=='S' || caracter[i]==A){
+        if(caracter[i]=='R' || caracter[i]=='S' || caracter[i]=='A'){
             printf("%c",caracter[i]);
             if(caracter[i]=='='){
             calculo=1;
@@ -62,9 +40,6 @@ void main()
     }
    }
   
-           
-   
-
       
    }
 
